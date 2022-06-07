99af58decc1c7f4ba11003352ae049a800094d01
Lucas Viccari <lviccari4@gmail.com>
2022-06-07 20:47:55 -0300

    Estilizando e corrigindo rotas

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 
+
 require('dotenv').config()
-const express = require('express');
+const express = require("express");
+const port = process.env.PORT || 3000;
+const res = require("express/lib/response");
 const app = express();
-const path = require('path');
-// // const { domainToASCII } = require('url');
-const port =  process.env.PORT || 3000 ;
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
+const path = require("path");
+const { runInNewContext } = require("vm");
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
 
 
 const portfolio = [
     {
-    id: 1 ,
+    id:1 ,
     nome: "Tênis Nike Air Max 90 Essential",
     imagem: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-max-90-essential-masculino-AJ1285-700-1.png",
     tamanho: "40",
     cor:"Caramelo",
-    descrição:"",
-    tipo:"",
-    peso:"",
-    material:""
+    descricao:"Tenis Air Max 90 Essential é simbolo de conforto , amortecimento de alta qualidade e uma beleza unica.",
+    tipo:"Casual",
+    material:"Camurça"
     },
     {
     id :2,
@@ -29,10 +30,9 @@ const portfolio = [
     imagem: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-force-1-low-retro-qs-masculino-AH1067-004-1.png",
     tamanho: "40",
     cor:"Preto",
-    descrição:"",
-    tipo:"",
-    peso:"",
-    material:""
+    descricao:"Tênis Air Force 1 Low Retro é representação de conforto e estilo , despojado e macio.",
+    tipo:"Casual",
+    material:"Nylon",
     },
     {
     id:3,
@@ -40,32 +40,32 @@ const portfolio = [
     imagem: "https://images.lojanike.com.br/500x500/produto/tenis-nike-air-jordan-i-mid-unissex-554724-061-1.png",
     tamanho: "40",
     cor:"Vermelho",
-    descrição:"",
-    tipo:"",
-    peso:"",
-    material:""
+    descricao:"Tênis Air Jordan , cobinação perfeita do old school com o design atlético, bom para passeio excelente pro bascket.",
+    tipo:"BascketBall",
+    material:"Couro"
     },
     ]
     
     
-let tenis = '';
-
+let tenis = undefined;
+let message = "";
 
 app.get('/', (req, res) => {
     
-    res.render('index' , {portfolio, tenis});
+    res.render('index' , {portfolio, tenis , message});
 })
 
 app.get('/produtos', (req, res) => {
      
-    res.render('produtos', {portfolio , tenis});
+    res.render('produtos', {portfolio , tenis,  message});
 
 })
+
 app.post("/cadastro", (req, res) => { 
     const tenis = req.body;
     tenis.id = portfolio.length + 1;
     portfolio.push(tenis);
-    // message = `Novo tenis criado com sucesso`
+    message = `Novo tenis criado com sucesso`
     res.redirect("/produtos");
 });
 
@@ -73,13 +73,28 @@ app.get("/detalhes/:id", (req, res) => {
     const id = +req.params.id;
     tenis = portfolio.find(tenis => tenis.id === id);
     res.redirect("/cadastro");
-  })
+  });
+
+  
+  app.post("/update/:id", (req, res) => {
+
+    const id = +req.params.id - 1;
+    const newTenis = req.body;
+    newTenis.id = id + 1;
+    portfolio[id] = newTenis;
+    tenis = undefined;
+    message = `Seu tenis foi editado com sucesso`
+    
+    res.redirect("/produtos");
+
+  });
+ 
 
   app.get("/delete/:id", (req, res) => { 
 
     const id = +req.params.id - 1;
     delete portfolio[id];
-   
+    message = `Tênis deletado`
     res.redirect("/produtos") ;
 });
 
--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,15 @@ function Mudarestado(el, id) {
     let display = document.querySelectorAll(el)[id - 1].style.display;
 
     if (display == "none") {
-        document.querySelectorAll(el)[id - 1].style.display = 'block';
+        document.querySelectorAll(el)[id - 1].style.display = 'flex';
     }
     else {
         document.querySelectorAll(el)[id - 1].style.display = 'none';
     }
 }
+const closeMessage = document.querySelector("#close")
+const message = document.querySelector("#message")
+
+closeMessage.addEventListener("click", function (){
+    message.style.display = "none"
+})
